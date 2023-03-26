fa472ae6718bd580877ceb6b90d715585ff777a9
burd5 <austinburdette5@gmail.com>
2023-03-26 13:59:02 -0600

    struggled to complete using slices. Looked at solution that was more concise

---
+++ swap_head_and_tail.py
@@ -0,0 +1,33 @@
+"""
+You need to swap the head and the tail of the specified array:
+
+the head (the first half) of array moves to the end, the tail (the second half) moves to the start. The middle element (if it exists) leaves on the same position.
+
+Return new array.
+
+For example:
+
+   [ 1, 2, 3, 4, 5 ]   =>  [ 4, 5, 3, 1, 2 ]
+    \----/   \----/         
+     head     tail 
+
+   [ -1, 2 ]  => [ 2, -1 ] 
+   [ 1, 2, -3, 4, 5, 6, -7, 8 ]   =>  [ 5, 6, -7, 8, 1, 2, -3, 4 ]  
+
+"""
+
+# my sloppy solution
+
+import math
+def swap_head_tail(arr):
+    mid = math.ceil(len(arr)/2)
+    if len(arr) % 2 == 0:
+        return arr[mid:] + arr[:mid]
+    else:
+        return arr[mid:] + [arr[mid - 1]] + arr[:mid - 1]
+
+# concise solution
+
+def swap_head_tail(arr):
+    i = len(arr) / 2
+    return arr[-i:] + arr[i:-i] + arr[:i]
