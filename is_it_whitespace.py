4884ccf0c207b7f6e7d064787fc66faeea4216b8
burd5 <austinburdette5@gmail.com>
2023-03-31 13:27:34 -0600

    completed challenge

---
+++ is_it_whitespace.py
@@ -0,0 +1,9 @@
+"""
+Implement String#whitespace?(str) (Ruby), String.prototype.whitespace(str) (JavaScript), String::whitespace(str) (CoffeeScript), or whitespace(str) (Python), which should return true/True if given object consists exclusively of zero or more whitespace characters, false/False otherwise.
+
+"""
+
+def whitespace(string):
+    return len(string.strip()) == 0
+
+
