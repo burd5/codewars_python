5938abdc78963fad13f470308e0b3c3aaab6f98e
burd5 <austinburdette5@gmail.com>
2023-04-23 17:15:31 -0600

    looked at alternate solution that preserved whitespace using ''.join at the beginning of statement

--- dropcaps.py
+++ dropcaps.py
@@ -1,7 +1,10 @@
 """
-DropCaps means that the first letter of the starting word of the paragraph should be in caps and the remaining lowercase, just like you see in the newspaper.
+DropCaps means that the first letter of the starting word of the paragraph 
+should be in caps and the remaining lowercase, just like you see in the newspaper.
 
-But for a change, let"s do that for each and every word of the given String. Your task is to capitalize every word that has length greater than 2, leaving smaller words as they are.
+But for a change, let"s do that for each and every word of the given String. 
+Your task is to capitalize every word that has length greater than 2, leaving 
+smaller words as they are.
 
 *should work also on Leading and Trailing Spaces and caps.
 
@@ -23,3 +26,8 @@ def drop_cap(str_):
             new_str += word
             
     return new_str
+
+# other solution
+
+def drop_cap(str_):
+    return ' '.join( w.capitalize() if len(w) > 2 else w for w in str_.split(' ') )
