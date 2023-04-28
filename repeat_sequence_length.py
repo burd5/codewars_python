0fa885cb15c8340dfff6498b6613ad271897a563
burd5 <austinburdette5@gmail.com>
2023-04-28 11:38:01 -0600

    looked at similar solution that used same process but used a helper function as well

--- repeat_sequence_length.py
+++ repeat_sequence_length.py
@@ -56,3 +56,16 @@ def repeat_sequence_len(n):
     # return the length of the array minus the index of the first instance it 
     # appeared (the difference is the solution)
     return len(sum_squares) - sum_squares.index(sum_)
+
+# similar solution that used helper function
+
+def F(n):
+    return sum(int(d)**2 for d in str(n))
+
+def repeat_sequence_len(n):
+    seq = [n]
+    c = F(n)
+    while c not in seq:
+        seq.append(c)
+        c = F(c)
+    return len(seq) - seq.index(c)
