1c0f67ec73b098ab2d69805139e9d9c871abc217
burd5 <austinburdette5@gmail.com>
2023-05-06 16:31:48 -0600

    completed challenge using list comprehension

---
+++ m_multiples_n.py
@@ -0,0 +1,16 @@
+"""
+Implement a function, multiples(m, n), which returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
+
+Ex.
+
+multiples(3, 5.0)
+should return
+
+[5.0, 10.0, 15.0]
+
+"""
+
+# my solution
+
+def multiples(m, n):
+    return [n * i for i in range(1, m + 1)]
