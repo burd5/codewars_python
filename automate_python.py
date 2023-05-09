215983caee05658102ca772f0d362d10c24ac54f
burd5 <austinburdette5@gmail.com>
2023-05-08 20:57:10 -0600

    added try except statement

--- automate_python.py
+++ automate_python.py
@@ -6,8 +6,14 @@ def collatz(number):
         print(3 * number + 1)
         return(3 * number + 1)
 
-value = collatz(int(input('Pick a number:')))
-
-while value != 1:
+try:
+    value = int(input('Pick a number:'))
     value = collatz(value)
+    while value != 1:
+        value = collatz(value)
+except ValueError:
+    print('Nums only plz')
+
+
+
 
