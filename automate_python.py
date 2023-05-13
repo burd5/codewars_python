3f46da536027f70bbeb1ae7bf4f819861e32d524
burd5 <austinburdette5@gmail.com>
2023-05-13 09:56:41 -0600

    added code for chess dict validator. Still need to run test cases

--- automate_python.py
+++ automate_python.py
@@ -17,7 +17,7 @@
 # Chapter 4 Practice Programs
 
 # Comma Code
-
+"""
 def comma_space(list):
     string = ''
     for i, word in enumerate(list):
@@ -55,9 +55,10 @@ def coin_flips():
     # Code that checks if there is a streak of 6 heads or tails in a row
     if 'HHHHHH' in flips or 'TTTTTT' in flips:
         number_of_streaks +=1
-
+"""
 # Character Picture Grid
 
+""" 
 grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['O', 'O', 'O', 'O', '.', '.'],
@@ -73,3 +74,106 @@ for i in range(6):
         if j < 8:
             print(grid[j][i], end='')
         else: print(grid[j][i])
+
+
+theBoard = {
+    'top-L': ' ',
+    'top-M': ' ',
+    'top-R': ' ',
+    'mid-L': ' ',
+    'mid-M': ' ',
+    'mid-R': ' ',
+    'low-L': ' ',
+    'low-M': ' ',
+    'low-R': ' '
+}
+
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+printBoard(theBoard)
+
+"""
+
+# Chess Dictionary Validator
+
+def isValidChessBoard(dict):
+    keys = '1a1b1c1d1e1f1g1h2a2b2c2d2e2f2g2h3a3b3c3d3e3f3g3h4a4b4c4d4e4f4g4h\
+    5a5b5c5d5e5f5g5h6a6b6c6d6e6f6g6h7a7b7c7d7e7f7g7h8a8b8c8d8e8f8g8h'
+
+    white = {
+        'pieces': 0,
+        'pawn': 0,
+        'rook': 0,
+        'bishop': 0,
+        'knight': 0,
+        'king': 0,
+        'queen': 0
+    }
+
+    black = {
+        'pieces': 0,
+        'pawn': 0,
+        'rook': 0,
+        'bishop': 0,
+        'knight': 0,
+        'king': 0,
+        'queen': 0
+    }
+
+    for k,v in dict.items():
+        # key value is not a valid square
+        if k not in keys: return False
+        # value starts with a w
+        elif v[0] == 'w':
+            pieces += 1
+            if white['pieces'] > 16 or \
+            white['pawn'] > 8 or \
+            white['rook'] > 2 or \
+            white['bishop'] > 2 or \
+            white['knight'] > 2 or \
+            white['king'] > 1 or \
+            white['queen'] > 1: return False
+            if 'pawn' in v:
+                white['pawn'] += 1
+            elif 'rook' in v:
+                white['rook'] += 1
+            elif 'bishop' in v:
+                white['bishop'] += 1
+            elif 'knight' in v:
+                white['knight'] += 1
+            elif 'queen' in v:
+                white['queen'] += 1
+            elif 'king' in v:
+                white['king'] += 1
+
+        # value starts with a b
+        elif v[0] == 'b':
+            pieces += 1
+            if black['pieces'] > 16 or \
+            black['pawn'] > 8 or \
+            black['rook'] > 2 or \
+            black['bishop'] > 2 or \
+            black['knight'] > 2 or \
+            black['king'] > 1 or \
+            black['queen'] > 1: return False
+            if 'pawn' in v:
+                black['pawn'] += 1
+            elif 'rook' in v:
+                black['rook'] += 1
+            elif 'bishop' in v:
+                black['bishop'] += 1
+            elif 'knight' in v:
+                black['knight'] += 1
+            elif 'queen' in v:
+                black['queen'] += 1
+            elif 'king' in v:
+                black['king'] += 1
+
+        # value did not start with w or b so must be an error
+        else: return False
+    # return True if no errors are found
+    return True
