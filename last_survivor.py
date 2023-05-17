ce9128e64ee0d3b9012e9a686a298c0097a49362
burd5 <austinburdette5@gmail.com>
2023-05-17 11:00:17 -0600

    completed challenge using for loop and slicing. Looked at solution that used pop() as well.

---
+++ last_survivor.py
@@ -0,0 +1,28 @@
+"""
+You are given a string of letters and an array of numbers.
+The numbers indicate positions of letters that must be removed, in order, starting from the beginning of the array.
+After each removal the size of the string decreases (there is no empty space).
+Return the only letter left.
+
+Example:
+
+let str = "zbk", arr = [0, 1]
+    str = "bk", arr = [1]
+    str = "b", arr = []
+    return 'b'
+
+"""
+
+# my solution
+
+def last_survivor(letters, coords): 
+    for i in coords:
+        letters = letters[:i] + letters[i+1:]
+    return letters
+
+# other solution
+
+def last_survivor(letters, coords): 
+    l= list(letters)
+    [l.pop(x) for x in coords]
+    return l[0]  
