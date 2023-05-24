fdf32dbc34f18413e2dece4fcd4c21a937983141
burd5 <austinburdette5@gmail.com>
2023-05-23 18:26:27 -0600

    completed challenge using strip and not in

---
+++ valid_spacing.py
@@ -0,0 +1,23 @@
+"""
+Your task is to write a function called valid_spacing() or validSpacing() which checks if a string has valid spacing. The function should return either true or false (or the corresponding value in each language).
+
+For this kata, the definition of valid spacing is one space between words, and no leading or trailing spaces. Words can be any consecutive sequence of non space characters. Below are some examples of what the function should return:
+
+* 'Hello world'   => true
+* ' Hello world'  => false
+* 'Hello world  ' => false
+* 'Hello  world'  => false
+* 'Hello'         => true
+
+"""
+
+# my solution
+
+def valid_spacing(s):
+    return '  ' not in s and s.strip() == s
+
+# other solution
+
+def valid_spacing(s):
+    return s == ' '.join(s.split())
+
