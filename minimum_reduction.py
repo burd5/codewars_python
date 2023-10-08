a05c61954b6a554768b024e1d445b53d3520b3d7
burd5 <austinburdette5@gmail.com>
2023-10-08 09:58:01 -0600

    added problem description

--- minimum_reduction.py
+++ minimum_reduction.py
@@ -2,7 +2,28 @@
 
 """
 
+Given an array of integers, find the minimum number of elements to remove from the array so that the square root of the largest integer in the array is less or equal to the smallest number in the same array.
 
+x = smallest integer in the array
+
+y = largest integer in the array
+
+Find the minimum number of elements to remove so, √y ≤ x.
+
+Example
+A = [3, 6, 5, 9, 16]
+min=3 max=16
+
+√16 > 3
+
+remove 16, so largest element becomes 9.
+
+√9 ≤ 3
+
+so
+
+min_remove(A) = 1
+since only 16 was removed.
 
 """
 
